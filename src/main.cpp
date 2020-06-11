7297fe94b24b0453c0720f023a38f58a2440ed51
MuhammadAbdelmoneim <menemmuhammad9@gmail.com>
2020-06-12 01:18:37 +0200

    clean main from test cases

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,8 @@
 #include "ncurses_display.h"
 #include "system.h"
-#include <dirent.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <string>
-#include <algorithm>
-#include <cerrno>
-#include <chrono>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <iterator>
-#include <sstream>
-#include <stdexcept>
-#include <string>
-#include <thread>
-#include <vector>
-#include "linux_parser.h"
-using std::string;
-using std::vector;
 
-const string s{"/"};
 
-class test
-{
-private:
-  int x;
-  /* data */
-public:
-  test(/* args */);
-  ~test
-();
-};
 
-test::test(/* args */)
-{
-  std::cout << "Constructor" << std::endl;
-}
-
-test::~test()
-{
-  std::cout << "De-Constructor" << std::endl;
-}
 
 int main() {
   System system;
